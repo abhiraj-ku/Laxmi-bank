613081aea8f585687f18fb47f025fde62635f882
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-07-25 16:55:40 +0530

    start with api

---
+++ api/account.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required, oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+
+	// Validate the input type with
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// 
+	
+
+}
--- api/server.go
+++ api/server.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	db "example/laxmi_chit_fund/db/sqlc"
+	"example/laxmi_chit_fund/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	config utils.Config
+	store  db.Store
+	router *gin.Engine
+}
+
+// func NewServer(store *db.Store) *Server{
+
+// }
+
+// Setup Gin Router to handle all the incoming API routes
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createAccount)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+
+}
